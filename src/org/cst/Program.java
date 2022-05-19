37a264ea230e5de58cf7dd9bd9ea34981b17ec80
Vengadesh97 <vengadeshw45@gmail.com>
2022-05-19 21:52:28 +0530

    Folder

--- src/org/cst/Program.java
+++ src/org/cst/Program.java
@@ -2,14 +2,14 @@ package org.cst;
 
 public class Program {
 
-	public static void tc1()
+	public static void tc2()
 	{
 		System.out.println(123);
 	}
 	
 	public static void main(String[] args) {
 		
-		tc1();
+		tc2();
 
 	}
 
